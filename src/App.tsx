66fe8e13de74f240482944c768954f26aa5bacc7
Moreland <TIM0416@daugherty.com>
2021-04-08 08:35:45 -0500

    starting to add realm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // React
-import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -2,24 +2,35 @@
 import 'react-native-gesture-handler';
 import React, { FC, ReactElement, ReactNode, useState } from 'react';
 
+// Realm
+import Realm, { Results } from "realm";
+import { Task } from '../realm/models/Tasks';
+
 export const TasksContext = React.createContext<ITasksContext | {}>({});
 
+const realm = new Realm({
+    path: "../realm",
+    schema: [Task],
+  });
+
 const TasksProvider: FC<ReactNode> = ({children}) : ReactElement => {
-    const [tasks, setTasks] = useState<ITaskList>([]);
+    const [tasks, setTasks] = useState<Results<Object>>(realm.objects("Task"););
     
-    const addTask = (task : ITask) : void => {
-        setTasks([...tasks, task]);
+    const addTask = (task) : void => {
+        let newTask;
+        realm.write(() => {
+            // Assign a newly-created instance to the variable.
+            newTask = realm.create("Task", task);
+          });
     };
 
-    const removeTask = (task : ITask) : void => {
-        const updatedTaskList : ITaskList = tasks.filter((e) => e._id !== task._id);
-        setTasks(updatedTaskList);
+    const removeTask = (task) : void => {
+        const myTask = realm.objectForPrimaryKey("Task", task._id);
+        realm.delete(myTask);
     };
 
-    const updateTask = (task : ITask) : void => {
-        const filteredTaskList : ITaskList = tasks.filter((e) => e._id !== task._id);
-        const updatedTaskList = [...filteredTaskList, task].sort((a, b) => a._id - b._id);
-        setTasks(updatedTaskList);
+    const updateTask = (task) : void => {
+        const t = realm.create(task,"Task", "modified");
     };
 
     return (
